7a2b7088d40b83992e93e7d89d6a7355c7a6007e
Oskar Janik <ojanik@invpro.co>
2018-09-07 11:57:40 +0200

    Added new function for daily updates

--- src/JiraWorklog.Importer/DailyImportFunction.cs
+++ src/JiraWorklog.Importer/DailyImportFunction.cs
@@ -6,23 +6,20 @@ using JiraWorklog.Core.DataTransferObjects;
 using JiraWorklog.Core.Models;
 using JiraWorklog.Core.Services.Implementation;
 using JiraWorklog.Data.AzureBlob.Stores;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
 
 namespace JiraWorklog.Importer
 {
-	public static class ImportFunction
+	public static class DailyImportFunction
 	{
 		private static AppSettings _appSettings;
 
-		[FunctionName("Import")]
-		public static async Task<IActionResult> Run(
-			[HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, TraceWriter log)
+		[FunctionName("DailyImportFunction")]
+		public static async Task Run([TimerTrigger("0 0 21 * * 1-5")]TimerInfo myTimer, ILogger log)
 		{
-			log.Info("Import started");
+			log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
 			_appSettings = new AppSettings
 			{
 				AtlassianAccount = GetEnvironmentVariable("AppSettings_AtlassianAccount"),
@@ -30,39 +27,31 @@ namespace JiraWorklog.Importer
 				ProjectCode = GetEnvironmentVariable("AppSettings_ProjectCode")
 			};
 
-			log.Info("Config read");
-			var parsedMonth = int.TryParse(req.Query["month"], out var month);
-			var parsedYear = int.TryParse(req.Query["year"], out var year);
+			log.LogInformation("Config read");
 
-			if (parsedMonth == false || parsedYear == false)
-				return new BadRequestObjectResult("Missing month or year parameter");
-			if (month < 1 || month > 12)
-				return new BadRequestObjectResult("Invalid month");
-			if (year < 2018)
-				return new BadRequestObjectResult("Invalid year");
+			var currentDate = DateTime.UtcNow;
 
-			var importedLogs = await ImportWorklogs(month, year, log);
-			var storedLogCount = await StoreWorklogs(month, year, importedLogs);
-			log.Info($"Stored {storedLogCount} worklogs");
-
-			return new OkObjectResult(storedLogCount);
+			var importedLogs = await ImportWorklogs(currentDate.Month, currentDate.Year, log);
+			var storedLogCount = await StoreWorklogs(currentDate.Month, currentDate.Year, importedLogs);
+			log.LogInformation($"Stored {storedLogCount} worklogs");
 		}
 
-		private static async Task<IEnumerable<IssueWorklog>> ImportWorklogs(int month, int year, TraceWriter log)
+		private static async Task<IEnumerable<IssueWorklog>> ImportWorklogs(int month, int year, ILogger log)
 		{
 			var startDate = new DateTime(year, month, 1, 0, 0, 0, DateTimeKind.Utc);
-			var endDate = startDate.AddMonths(2);
-			log.Info($"Import logs between {startDate.ToShortDateString()} " +
-				$"and {endDate.ToShortDateString()}");
+			var endDate = startDate.AddMonths(1).AddSeconds(-1);
+
+			log.LogInformation($"Import logs between {startDate.ToShortDateString()} " +
+					 $"and {endDate.ToShortDateString()}");
 
 			var service = new ImportWorklogService(_appSettings);
 			var results = await service.Import(startDate, endDate);
 			var count = results.Count;
-			log.Info($"Found {count} worklogs");
+			log.LogInformation($"Found {count} worklogs");
 			return results;
 		}
 
-		private static async Task<int> StoreWorklogs(int month, int year, IEnumerable<IssueWorklog>worklogs)
+		private static async Task<int> StoreWorklogs(int month, int year, IEnumerable<IssueWorklog> worklogs)
 		{
 			var list = new List<WorklogDto>();
 
